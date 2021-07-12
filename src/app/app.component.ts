4579fe9c34deb5dacd51f8837df4adcd0ec88c5c
Liem <leliem28@gmail.com>
2021-07-12 21:27:34 +0700

    Finish bai 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularBai6';
+  products: Product[] = [];
+  
+  addNewProduct(maSP: string, tenSP: string, gia: any) {
+    let product: Product = {maSP, tenSP, gia};
+    this.products.push(product);
+  }
+
 }
+
+interface Product {
+  maSP: string;
+  tenSP: string;
+  gia: number;
+}
+
